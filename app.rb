8f3f9a626e8e06f12e2596933fa5e87c3d77bc8b
Andrew Fontaine <andrew@afontaine.ca>
2015-02-20 17:25:34 -0700

    Only enable Reloader for dev envs

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ Bundler.require(:default)
 Bundler.require(:development) if development?
 
 class MyApp < Sinatra::Base
+	configure :development do
+		register Sinatra::Reloader
+	end
 
 	get '/' do
 		'Hello everyone!'
