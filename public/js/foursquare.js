18204e9442190692157f985fbbf884889200e24b
Andrew Fontaine <andrew@afontaine.ca>
2015-02-20 21:51:26 -0700

    JS can take in a location and a type
    Current types are hotel and convention centres

--- public/js/foursquare.js
+++ public/js/foursquare.js
@@ -1,12 +1,17 @@
-function fourSquare(location) {
+var category_map = {
+    'hotel': '4bf58dd8d48988d1fa931735',
+    'convention': '4bf58dd8d48988d1ff931735'
+};
+
+function fourSquare(location, category) {
     console.log(location);
     $.get(
         'https://api.foursquare.com/v2/venues/search' +
         '?client_id=BNDGPC13FY511GOP2TNMHF41AMYDYCGOHNRFM1NRGUC5BIQH' +
         '&client_secret=4WYPCXAI1CJ4NA23WVOFWFMU1IUMFY554JMYQNKBWP1PKFEX' +
-        '&v=20140220' +
+        '&v=20150220' +
         '&near=' + encodeURI(location) +
-        '&categoryId=4bf58dd8d48988d1fa931735',
+        '&categoryId=' + category_map[category],
         function(data) {
             console.log(data);
         }
