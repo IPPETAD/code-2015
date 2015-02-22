841e8bd15557ac258a2ed3d793fe11fbbb3900f5
James Finlay <jtfinlay@ualberta.ca>
2015-02-22 21:00:32 +0000

    Tour is done!!

--- public/js/introjs/home.js
+++ public/js/introjs/home.js
@@ -19,8 +19,12 @@ $(".growth").attr('data-intro',
 	  "<p>Here you can view industry field growth.</p>"
 	+ "<p>Detailed growth analytics provide an easy way to inspect industries across Canada. We not only provides a simple and sleek way to visualize industry growth, but provide it in a manner that is actually useful - especially for those looking to host conferences or enter new business areas.</p>");
 
+$(".navbar-right").attr('data-step', 5);
+$(".navbar-right").attr('data-intro',
+	  "<p>We also provide working signup and authentication.</p>"
+	+ "<p>However, nothing in this prototype is restricted, so there's no need to register.</p>");
 
-$(".title").attr("data-step", 5);
+$(".title").attr("data-step", 6);
 $(".title").attr("data-position", "top");
 $(".title").attr('data-intro',
 	  "<p>Alright, time to move on!</p>"
