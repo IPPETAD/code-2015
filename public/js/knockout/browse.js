77e8ffbd4bf0182e1031a66f556dbaeae19030e1
Jacob Viau <jviau@ualberta.ca>
2015-02-22 11:34:36 -0700

    Browse filter refactored

--- public/js/knockout/browse.js
+++ public/js/knockout/browse.js
@@ -7,8 +7,30 @@ function BrowseViewModel() {
         return self.conferences.filter(function(conf) {
             if(!self.filter() || self.filter() == '' || self.filter().length < 3)
                 return true;
-            return conf.title().toLowerCase().indexOf(self.filter().toLowerCase()) > -1 ||
-                conf.description().toLowerCase().indexOf(self.filter().toLowerCase()) > -1;
+
+            var filters = splitFilter(self.filter());
+
+            // If single item, filter on title and description
+            if (filters.length == 1 && typeof filters[0] == 'string') {
+              return conf.title().toLowerCase().indexOf(filters[0].toLowerCase()) > -1 ||
+                conf.description().toLowerCase().indexOf(filters[0].toLowerCase()) > -1;
+            }
+
+            // multiple items, check if field exists and filter on it
+            return filters.some(function(filter) {
+              if (typeof filter == 'string') {
+                // ignore stuff without a property
+                return false;
+              }
+
+              // oh god it is ugly
+              if (filter.length > 1 && typeof filter[0] == 'string' && conf[filter[0]] && conf[filter[0]]().indexOf) {
+                return filter[1].some(function(value) {
+                  return conf[filter[0]]().toLowerCase().indexOf(value) > -1;
+                })
+              }
+              return false;
+            });
         }).extend({paging: 10});
     }, this);
 
@@ -31,15 +53,11 @@ function BrowseViewModel() {
     });
 
     self.filter.subscribe(function(filter) {
-      if (!filter || filter == '') {
-        window.location.hash = '';
-      } else {
-        window.location.hash = "filter=" + filter;
-      }
+      window.location.hash = filter;
     });
 
     Sammy(function() {
-      this.get('#filter=:filter', function() {
+      this.get('#:filter', function() {
         self.filter(this.params.filter);
       });
     }).run();
@@ -48,3 +66,21 @@ function BrowseViewModel() {
 $(function() {
     ko.applyBindings(new BrowseViewModel())
 });
+
+function splitFilter(filter) {
+  if (!filter) {
+    return;
+  }
+
+  return decodeURIComponent(filter)
+    .toLowerCase()
+    .split('&').map(function(item) {
+      if (item.indexOf('=') > -1) {
+        item = item.split('=');
+        if (item.length == 2) {
+          item[1] = item[1].split(' ');
+        }
+      }
+      return item;
+  });
+}
